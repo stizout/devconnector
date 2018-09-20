ddecea1f78913c4a47d62df5e79ae1001e14ba00
Alex Stout <Alex@Alexs-MBP.domain>
2018-09-20 13:06:50 -0700

    can log in and out

--- validation/login.js
+++ validation/login.js
@@ -15,11 +15,11 @@ module.exports = function validateLoginInput(data) {
   data.password = !isEmpty(data.password) ? data.password : ''
 
 
-  if(!validator.isEmpty(data.email)) {
-    errors.email = 'Email field is required'
-  }
-  if(validator.isEmail(data.email)) {
+  if(!validator.isEmail(data.email)) {
     errors.email = 'Email must be a valid email address'
+  }
+  if(validator.isEmpty(data.email)) {
+    errors.email = 'Email field is required'
   }
   if(validator.isEmpty(data.password)) {
     errors.password = 'Password field is required'
