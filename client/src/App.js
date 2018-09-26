9d59c37c1bb7ad1c4c516ca40b4e9790a1174b5c
Alex Stout <Alex@Alexs-MBP.domain>
2018-09-26 12:30:43 -0700

    install react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Landing from './components/Landing';
+import Login from './components/Login';
+import Register from './components/Register';
+import Footer from './components/Footer';
+import './style.css'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route path="/" exact component={Landing}/>
+            <div>
+              <Route path="/register" component={Register}/>
+              <Route path="/login" component={Login}/>
+            </div>
+          <Footer />
+        </div>
+      </Router>
     );
   }
 }
---
+++ client/src/components/Register.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class Register extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Register page</h1>
+      </div>
+    )
+  }
+}
+
+
+
+
+export default Register
