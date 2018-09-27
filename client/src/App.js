de486f221a63172fee33647c8a2304828d66c40e
Alex Stout <Alex@Alexs-MBP.domain>
2018-09-27 10:02:29 -0700

    setup redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { Provider } from 'react-redux';
+import store from '../src/ducks/store';
 import Navbar from './components/Navbar'
 import Landing from './components/Landing';
 import Login from './components/Login';
@@ -7,20 +9,24 @@ import Register from './components/Register';
 import Footer from './components/Footer';
 import './style.css'
 
+
+
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <Navbar />
-          <Route path="/" exact component={Landing}/>
-            <div>
-              <Route path="/register" component={Register}/>
-              <Route path="/login" component={Login}/>
-            </div>
-          <Footer />
-        </div>
-      </Router>
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <Route path="/" exact component={Landing}/>
+              <div>
+                <Route path="/register" component={Register}/>
+                <Route path="/login" component={Login}/>
+              </div>
+            <Footer />
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,10 +1,63 @@
 import React, { Component } from 'react'
 
 class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: ''
+    }
+  }
+
+  handleInputs = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    let loginUser = {
+      email: this.state.email,
+      password: this.state.password
+    }
+
+    console.log(loginUser)
+  }
+
   render() {
     return (
-      <div>
-        <h1>Login page</h1>
+      <div className="login">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 m-auto">
+              <h1 className="display-4 text-center">Log In</h1>
+              <p className="lead text-center">Sign in to your DevConnector account</p>
+              <form action="dashboard.html" onSubmit={this.onSubmit}>
+                <div className="form-group">
+                  <input 
+                    type="email" 
+                    className="form-control form-control-lg" 
+                    placeholder="Email Address" 
+                    name="email"
+                    value={this.state.email}
+                    onChange={this.handleInputs}
+                />
+                </div>
+                <div className="form-group">
+                  <input 
+                    type="password" 
+                    className="form-control form-control-lg" 
+                    placeholder="Password" name="password"
+                    value={this.state.password}
+                    onChange={this.handleInputs}
+                  />
+                </div>
+                <input type="submit" className="btn btn-info btn-block mt-4" />
+              </form>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
